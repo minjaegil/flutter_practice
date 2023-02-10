fb55dfe91f496552e26998e6cd9d2cd04a870f01
minjaegil <minjaegil@gmail.com>
2023-02-09 19:21:40 -0600

    modified auth button

---
+++ tiktok_clone/lib/features/authentication/login_screen.dart
@@ -0,0 +1,69 @@
+import 'package:flutter/material.dart';
+import 'package:tiktok_clone/constants/gaps.dart';
+import 'package:tiktok_clone/constants/sizes.dart';
+
+class LoginScreen extends StatelessWidget {
+  const LoginScreen({super.key});
+
+  void _onSignUpTap(BuildContext context) {
+    // Here we don't use push because we want to go 'back'
+    // We don't want to add the previous screen
+    Navigator.of(context).pop(); //remove the most recent screen
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: Sizes.size40),
+          child: Column(
+            children: const [
+              Gaps.v80,
+              Text(
+                "Log in to TikTok",
+                style: TextStyle(
+                  fontSize: Sizes.size24,
+                  fontWeight: FontWeight.w700,
+                ),
+              ),
+              Gaps.v20,
+              Text(
+                "Manage your account, check notifications, comment on videos, and more.",
+                style: TextStyle(
+                  fontSize: Sizes.size16,
+                  color: Colors.black45,
+                ),
+                textAlign: TextAlign.center,
+              ),
+              Gaps.v40,
+            ],
+          ),
+        ),
+      ),
+      bottomNavigationBar: BottomAppBar(
+          color: Colors.grey.shade50,
+          elevation: 1, // shadow
+          child: Padding(
+            padding: const EdgeInsets.symmetric(vertical: Sizes.size32),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                const Text("Don't have an account?"),
+                Gaps.h5,
+                GestureDetector(
+                  onTap: () => _onSignUpTap(context),
+                  child: Text(
+                    "Sign up",
+                    style: TextStyle(
+                      color: Theme.of(context).primaryColor,
+                      fontWeight: FontWeight.w600,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          )),
+    );
+  }
+}
---
+++ tiktok_clone/lib/features/authentication/username_screen.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+import 'package:tiktok_clone/constants/gaps.dart';
+import 'package:tiktok_clone/constants/sizes.dart';
+
+class UsernameScreen extends StatelessWidget {
+  const UsernameScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        title: const Text('Sign up'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: Sizes.size36),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Gaps.v40,
+            const Text(
+              "Create username",
+              style: TextStyle(
+                fontSize: Sizes.size24,
+                fontWeight: FontWeight.w700,
+              ),
+            ),
+            Gaps.v8,
+            const Text(
+              "You can always change this later.",
+              style: TextStyle(
+                fontSize: Sizes.size16,
+                color: Colors.black54,
+              ),
+            ),
+            Gaps.v16,
+            TextField(
+              cursorColor: Theme.of(context).primaryColor,
+              decoration: const InputDecoration(
+                hintText: "Username",
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
